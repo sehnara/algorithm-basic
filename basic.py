f97290ca69d4363e8cff56aacf44366a5c363b19
sehoon <gsh723@naver.com>
2021-02-23 13:33:11 +0900

    basic algorithm : 1.print, 2.input

---
+++ basic.py
@@ -0,0 +1,54 @@
+# 10문제씩 commit
+
+# code up basic 100
+
+# 1. 출력
+# 6008
+# print('print("Hello\\nWorld")')
+
+# 2. 입출력
+# 6009
+# f = float(input())
+# print(f)
+
+# 6012 : 두 변수 입력(줄 바꿔 입력)
+# i1 = input()
+# i2 = input()
+# print(i2)
+# print(i1)
+
+# 6014 : 반복 출력
+# f = float(input())
+# for i in range(3):
+#     print(f)
+
+# 6015 : 두 변수 입력(공백으로 구분 입력)
+# a, b = input().split(" ")
+# print("%s %s" % (b, a))
+
+# 6017 : 똑같은 문장 '공백'주고 반복 출력 *
+# s = input()
+# print(s, s, s)
+# print("%s %s %s" % (s, s, s))
+
+# 6018 : 시간 입출력
+# y, m, d = input().split(".")
+# print("%s-%s-%s" % (d, m, y))
+
+# 6019
+# a, b = input().split("-")
+# print("%s%s" % (a, b))
+
+# 6021 : 5개 문자로 이루어진 단어 줄바꿔 출력
+# a = input()
+
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+# 6022
+# s = input().split(" ")
+# # print("%s%s" % (s[0], s[1]))
+# print("%s" % (s[0]+s[1]))
