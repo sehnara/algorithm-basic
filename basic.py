6104a8f6f3180ab4d6f2e0cb143cfb673c4027c8
sehoon <gsh723@naver.com>
2021-02-23 18:44:50 +0900

    3. 값변환, 4.산술연산 5. 비교연산

--- basic.py
+++ basic.py
@@ -2,11 +2,11 @@
 
 # code up basic 100
 
-# 1. 출력
+# 1. 출력 ----- ----- ----- ----- ----- -----
 # 6008
 # print('print("Hello\\nWorld")')
 
-# 2. 입출력
+# 2. 입출력 ----- ----- ----- ----- ----- -----
 # 6009
 # f = float(input())
 # print(f)
@@ -52,3 +52,68 @@
 # s = input().split(" ")
 # # print("%s%s" % (s[0], s[1]))
 # print("%s" % (s[0]+s[1]))
+
+
+# 3. 변환 ----- ----- ----- ----- ----- -----
+# 1) 진수 변환
+# 2) 데이터 타입 변환
+
+# 6025 : 16진수로 입력
+# a = int(input(), 16)
+# print("{0:o}".format(a))
+
+# 6030 : 영문자 입력, 10진수 변환 (ord => 문자를 숫자로 // chr => 숫자를 문자로)
+# a = ord(input())
+# b = chr(a+1)
+# print(b)
+
+# 6033 : 문자 입력 후 그 다음 문자 출력
+# 사고 흐름 : '다음'이 키워드 => 순서 => 숫자 = 문자를 어떻게 숫자화하지?
+# a = ord(input())
+# b = chr(a+1)
+# print(b)
+
+# 6034 : 같은 문자 여러번 출력 **
+# >> 원래 내 방식
+# a, b = input().split(" ")
+# b = int(b)
+# for i in range(b):
+#     print("{0}".format(a), end="")
+
+# >> 간단한 방식
+# a, b = input().split(" ")
+# print(a*int(b))
+
+# 3. 산술 연산 ----- ----- ----- ----- ----- -----
+# 6038
+# a, b = map(int, input().split(" "))
+# print(a % b)
+
+# 6042 : 반올림
+# a = float(input())
+# print(round(a, 2))
+
+# 6047 : 비트시프트 연산(2배는 기본으로 깔고 드감) *
+# a<<1 : a를 2배
+# a>>1 : a를 1/2배
+# a<<2 : a를 4배
+# a, b = map(int, input().split())
+# print(a << b)
+
+
+# boolean을 다루는 예약어 : not, and, or
+# 6048 : true -> false, false -> true "not" *
+# >> 원래 내 방식
+# a = int(input())
+# if a == 0:
+#     print(bool(1))
+# else:
+#     print(bool(0))
+
+# >>간단한 방식
+# a = bool(int(input()))
+# print(not a)
+
+# 6054
+# a, b = map(int, input().split(" "))
+# print(bool(a) or bool(b))
